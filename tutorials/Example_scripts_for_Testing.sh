05e1bf0482a8fd9d6e11b6c5a864eab0e38732d4
tanfiona <pillowtann@gmail.com>
2022-08-19 16:38:51 +0800

    Improve descriptions

--- tutorials/Example_scripts_for_Testing.sh
+++ tutorials/Example_scripts_for_Testing.sh
@@ -1,9 +1,14 @@
 # You may call the pretrained models to test on your own datasets directly.
-# Only caveat is they need to be processed into our CSV dataset format.
-# In this file, we demonstrate how to "test" on the Causal News Corpus, a shared task of CASE 2022 @ EMNLP.
+
+# Only caveat is they need to be processed into our CSV dataset format. 
+# We created a helper function "_datasets/add_dummy_columns.py" to help. 
+# But it is not perfect, especially for columns with different naming.
+
+# In this file, we demonstrate how to "test" directly on an external corpus.
+# We use the Causal News Corpus, a shared task of CASE 2022 @ EMNLP.
 # Data source: https://github.com/tanfiona/CausalNewsCorpus/tree/master/data
 
-### Subtask 1 ### 
+### Subtask 1 : Sequence Classification ### 
 
 sudo /home/fiona/anaconda3/envs/torchgeom/bin/python3 \
 _datasets/add_dummy_columns.py --input_csv_path data/test_subtask1_text.csv
@@ -15,7 +20,8 @@ run_seqbase.py \
 --output_dir outs/causalnewscorpus_subtask1 \
 --per_device_eval_batch_size 32 --seed 42
 
-### Subtask 2 ###
+### Subtask 2 : Cause-Effect Span Detection ###
+# Note that the original task also identifies "Signal" spans, but we ignore them for now.
 
 sudo /home/fiona/anaconda3/envs/torchgeom/bin/python3 \
 _datasets/add_dummy_columns.py --input_csv_path data/test_subtask2_text.csv
